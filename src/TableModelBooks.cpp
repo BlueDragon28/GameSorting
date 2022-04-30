ddbb27303dfe5282d0e45ccefa7ddc13e42195e3
Erwan Saclier de la Bâtie <erwansaclierdelabatie@yahoo.fr>
2022-04-30 16:47:09 +0200

    fix books url misleading QString::arg on SQL statement

--- src/TableModelBooks.cpp
+++ src/TableModelBooks.cpp
@@ -1230,11 +1230,11 @@ void TableModelBooks::setUrl(const QModelIndex& index, const QString& url)
 
         QString statement = QString(
             "UPDATE \"%1\"\n"
-            "SET Url = \"%2\"\n"
-            "WHERE BooksID = %3;")
+            "SET Url = \"%3\"\n"
+            "WHERE BooksID = %2;")
                 .arg(m_tableName)
-                .arg(url)
-                .arg(m_data.at(index.row()).bookID);
+                .arg(m_data.at(index.row()).bookID)
+                .arg(url);
 
 #ifndef NDEBUG
         std::cout << statement.toLocal8Bit().constData() << "\n" << std::endl;
